d40748e3a29e24d435e8e63fcda7da95a577566f
liuyanqi <liuyq@gszygroup.com>
2017-03-22 14:26:27 +0800

    提交代码 java8形式

--- src/main/java/tk/geniusman/fx/controller/MainLayoutController.java
+++ src/main/java/tk/geniusman/fx/controller/MainLayoutController.java
@@ -17,9 +17,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.StageStyle;
-import tk.geniusman.listener.ChangedListener;
-import tk.geniusman.listener.FinishedListener;
-import tk.geniusman.listener.ProcessChangedListener;
 import tk.geniusman.main.ForkJoinDownload;
 import tk.geniusman.manager.Manager;
 import tk.geniusman.manager.UIManager;
@@ -33,195 +30,183 @@ import tk.geniusman.manager.UIManager;
 // @SuppressWarnings("restriction")
 public class MainLayoutController {
 
-	@FXML
-	private TextField address; // download URL address
+    @FXML
+    private TextField address; // download URL address
 
-	@FXML
-	private TextField localAddress; // local IP address
+    @FXML
+    private TextField localAddress; // local IP address
 
-	@FXML
-	private Button open;
+    @FXML
+    private Button open;
 
-	@FXML
-	private Button download;
+    @FXML
+    private Button download;
 
-	@FXML
-	private Button pauseOrResume;
+    @FXML
+    private Button pauseOrResume;
 
-	@FXML
-	private Pane processPane;
+    @FXML
+    private Pane processPane;
 
-	@FXML
-	private Pane root;
+    @FXML
+    private Pane root;
 
-	@FXML
-	private ProgressBar process;
+    @FXML
+    private ProgressBar process;
 
-	@FXML
-	private Label speedLab;
+    @FXML
+    private Label speedLab;
 
-	@FXML
-	private Label percentLab;
+    @FXML
+    private Label percentLab;
 
-	@FXML
-	private Button terminate;
+    @FXML
+    private Button terminate;
 
-	@FXML
-	private AnchorPane taskListPane;
+    @FXML
+    private AnchorPane taskListPane;
 
-	/** Rectangle object array */
-	private Rectangle[][] array; // save the Rectangle object to array
-	private static final int WIDTH = 100;
-	private static final int HEIGHT = 100;
-	// private static final int PIXELS = WIDTH * HEIGHT;
-	private final Manager m = Manager.getInstance();
+    /** Rectangle object array */
+    private Rectangle[][] array; // save the Rectangle object to array
+    private static final int WIDTH = 100;
+    private static final int HEIGHT = 100;
+    // private static final int PIXELS = WIDTH * HEIGHT;
+    private final Manager m = Manager.getInstance();
 
-	/**
-	 * constructor
-	 */
-	public MainLayoutController() {
-	}
+    /**
+     * constructor
+     */
+    public MainLayoutController() {
+    }
 
-	@FXML
-	private void initialize() {
-		address.setText("http://down.360safe.com/cse/360cse_8.5.0.126.exe");
-		localAddress.setText("d:\\tools\\");
-		pauseOrResume.setDisable(true);
+    @FXML
+    private void initialize() {
+        address.setText("http://down.360safe.com/cse/360cse_8.5.0.126.exe");
+        localAddress.setText("d:\\tools\\");
+        pauseOrResume.setDisable(true);
 
-		this.array = new Rectangle[WIDTH][HEIGHT];
-		UIManager uiManager = UIManager.newInstance(array, process, speedLab, percentLab, processPane);
-		uiManager.init();
+        this.array = new Rectangle[WIDTH][HEIGHT];
+        UIManager uiManager = UIManager.newInstance(array, process, speedLab, percentLab, processPane);
+        uiManager.init();
 
-		m.addListener(new ChangedListener() {
-			@Override
-			public void change(final long current, final Thread t) {
-				uiManager.changeColor(current, m.getSize());
-			}
-		});
+        // add change Color listener
+        m.addListener((current, t) -> Platform.runLater(() -> uiManager.changeColor(current, m.getSize())));
 
-		m.addProcessListener(new ProcessChangedListener() {
-			@Override
-			public void change(double rate, long speed, Thread t) {
-				Platform.runLater(() -> {
-					uiManager.changePercent(rate, speed);
-				});
-			}
-		});
+        // add change Percent listener
+        m.addProcessListener((rate, speed, t) -> Platform.runLater(() -> uiManager.changePercent(rate, speed)));
 
-		m.addFinishListener(new FinishedListener() {
-			@Override
-			public void finish(boolean hasError, String message) {
-				Platform.runLater(() -> {
-					download.setDisable(false);
-					pauseOrResume.setText("Pause");
-					pauseOrResume.setDisable(true);
-					showAlert("File Download Tools", message,
-							hasError ? Alert.AlertType.ERROR : Alert.AlertType.INFORMATION);
-				});
-			}
-		});
-	}
+        // add finish listener
+        m.addFinishListener((hasError, message) -> {
+            Platform.runLater(() -> {
+                download.setDisable(false);
+                pauseOrResume.setText("Pause");
+                pauseOrResume.setDisable(true);
+                showAlert("File Download Tools", message,
+                        hasError ? Alert.AlertType.ERROR : Alert.AlertType.INFORMATION);
+            });
+        });
+    }
 
-	/**
-	 * the action for handle the button of Download
-	 */
-	@FXML
-	private void handleDownload() {
-		String addressTxt = address.getText();
-		if (addressTxt == null || addressTxt.isEmpty()) {
-			showAlert("File Download Tools", "address URL must be specified..", Alert.AlertType.ERROR);
-			return;
-		}
+    /**
+     * the action for handle the button of Download
+     */
+    @FXML
+    private void handleDownload() {
+        String addressTxt = address.getText();
+        if (addressTxt == null || addressTxt.isEmpty()) {
+            showAlert("File Download Tools", "address URL must be specified..", Alert.AlertType.ERROR);
+            return;
+        }
 
-		String localAddressTxt = localAddress.getText();
-		if (localAddressTxt == null || localAddressTxt.isEmpty()) {
-			showAlert("File Download Tools", "Local saved Path must be specified..", Alert.AlertType.ERROR);
-			return;
-		}
+        String localAddressTxt = localAddress.getText();
+        if (localAddressTxt == null || localAddressTxt.isEmpty()) {
+            showAlert("File Download Tools", "Local saved Path must be specified..", Alert.AlertType.ERROR);
+            return;
+        }
 
-		clearColor();
-		final String[] args = { addressTxt, "15", localAddressTxt, null };
-		new Thread(() -> {
-			try {
-				ForkJoinDownload.main(args);
-				System.out.println("Clear..");
-				m.clear();
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
-		}).start();
+        clearColor();
+        final String[] args = { addressTxt, "15", localAddressTxt, null };
+        new Thread(() -> {
+            try {
+                ForkJoinDownload.main(args);
+                System.out.println("Clear..");
+                m.clear();
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }).start();
 
-		download.setDisable(true);
-		pauseOrResume.setDisable(false);
-	}
+        download.setDisable(true);
+        pauseOrResume.setDisable(false);
+    }
 
-	/**
-	 * The action for handle the button of Open File
-	 */
-	@FXML
-	private void handleOpen() {
-		final DirectoryChooser dChooser = new DirectoryChooser();
-		dChooser.setTitle("Choose the Saved Path");
+    /**
+     * The action for handle the button of Open File
+     */
+    @FXML
+    private void handleOpen() {
+        final DirectoryChooser dChooser = new DirectoryChooser();
+        dChooser.setTitle("Choose the Saved Path");
 
-		File defaultDirectory = new File("d:\\");
-		dChooser.setInitialDirectory(defaultDirectory);
-		File file = dChooser.showDialog(open.getScene().getWindow());
-		if (file != null) {
-			localAddress.setText(file.getAbsolutePath());
-		}
-	}
+        File defaultDirectory = new File("d:\\");
+        dChooser.setInitialDirectory(defaultDirectory);
+        File file = dChooser.showDialog(open.getScene().getWindow());
+        if (file != null) {
+            localAddress.setText(file.getAbsolutePath());
+        }
+    }
 
-	@FXML
-	private void handlePauseOrResume() {
-		if (m.isPause()) {
-			m.resume();
-			pauseOrResume.setText("Pause");
-		} else {
-			m.setPause(true);
-			pauseOrResume.setText("Continue");
-		}
-	}
+    @FXML
+    private void handlePauseOrResume() {
+        if (m.isPause()) {
+            m.resume();
+            pauseOrResume.setText("Pause");
+        } else {
+            m.setPause(true);
+            pauseOrResume.setText("Continue");
+        }
+    }
 
-	@FXML
-	private void handleTerminate() {
-		Optional<ButtonType> ret = showAlert("File Download Tools", "Confirm to terminate..",
-				Alert.AlertType.CONFIRMATION);
-		if (ret.get() == ButtonType.OK) {
-			Manager.getInstance().terminate();
-		}
-	}
+    @FXML
+    private void handleTerminate() {
+        Optional<ButtonType> ret = showAlert("File Download Tools", "Confirm to terminate..",
+                Alert.AlertType.CONFIRMATION);
+        if (ret.get() == ButtonType.OK) {
+            Manager.getInstance().terminate();
+        }
+    }
 
-	/**
-	 * clearColor
-	 */
-	private void clearColor() {
-		if (array == null) {
-			return;
-		}
-		for (int j = 0; j < WIDTH; j++) {
-			for (int i = 0; i < HEIGHT; i++) {
-				final Rectangle r = array[j][i];
-				if (r != null) {
-					r.setFill(Color.CORNSILK);
-				}
-			}
-		}
-	}
+    /**
+     * clearColor
+     */
+    private void clearColor() {
+        if (array == null) {
+            return;
+        }
+        for (int j = 0; j < WIDTH; j++) {
+            for (int i = 0; i < HEIGHT; i++) {
+                final Rectangle r = array[j][i];
+                if (r != null) {
+                    r.setFill(Color.CORNSILK);
+                }
+            }
+        }
+    }
 
-	/**
-	 * showAlert
-	 * 
-	 * @param title
-	 * @param message
-	 * @param type
-	 */
-	private Optional<ButtonType> showAlert(String title, String message, Alert.AlertType type) {
-		final Alert a = new Alert(type);
-		a.initStyle(StageStyle.UNDECORATED);
-		a.setTitle(title);
-		a.setHeaderText(type.name());
-		a.setResizable(false);
-		a.setContentText(message);
-		return a.showAndWait();
-	}
+    /**
+     * showAlert
+     * 
+     * @param title
+     * @param message
+     * @param type
+     */
+    private Optional<ButtonType> showAlert(String title, String message, Alert.AlertType type) {
+        final Alert a = new Alert(type);
+        a.initStyle(StageStyle.UNDECORATED);
+        a.setTitle(title);
+        a.setHeaderText(type.name());
+        a.setResizable(false);
+        a.setContentText(message);
+        return a.showAndWait();
+    }
 }
--- src/main/java/tk/geniusman/fx/main/MainGui.java
+++ src/main/java/tk/geniusman/fx/main/MainGui.java
@@ -3,22 +3,16 @@ package tk.geniusman.fx.main;
 import java.awt.AWTException;
 import java.awt.PopupMenu;
 import java.awt.TrayIcon;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import com.sun.javafx.application.PlatformImpl;
-
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -32,171 +26,137 @@ import tk.geniusman.manager.Manager;
  */
 public class MainGui extends Application {
 
-	private Stage primaryStage;
-	private Scene rootScene;
-	private TrayIcon trayIcon;
+    private Stage primaryStage;
+    private Scene rootScene;
+    private TrayIcon trayIcon;
 
-	private volatile double xOffset = 0;
-	private volatile double yOffset = 0;
+    private volatile double xOffset = 0;
+    private volatile double yOffset = 0;
 
-	/**
-	 * the main entrance of program
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
+    /**
+     * the main entrance of program
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.initStyle(StageStyle.UNDECORATED);
-		Platform.setImplicitExit(false);
+    @SuppressWarnings("restriction")
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Platform.setImplicitExit(false);
 
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Java Download Tools");
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Java Download Tools");
 
-		setUserAgentStylesheet(STYLESHEET_MODENA);
-		System.setProperty("com.sun.javafx.highContrastTheme", "BLACKONWHITE");
-		PlatformImpl.setAccessibilityTheme("High Contrast #2");
+        setUserAgentStylesheet(STYLESHEET_MODENA);
+        System.setProperty("com.sun.javafx.highContrastTheme", "BLACKONWHITE");
+        com.sun.javafx.application.PlatformImpl.setAccessibilityTheme("High Contrast #2");
 
-		try {
-			// Load the root layout from the FXML file
-			FXMLLoader mainLayoutLoader = new FXMLLoader(MainGui.class.getResource("/ui/MainLayout2.fxml"));
-			Pane rootLayout = mainLayoutLoader.load();
-			setDragable(rootLayout);
+        try {
+            // Load the root layout from the FXML file
+            FXMLLoader mainLayoutLoader = new FXMLLoader(MainGui.class.getResource("/ui/MainLayout2.fxml"));
+            Pane rootLayout = mainLayoutLoader.load();
+            setDragable(rootLayout);
 
-			rootScene = new Scene(rootLayout);
-			primaryStage.setScene(rootScene);
-			primaryStage.setResizable(false);
+            rootScene = new Scene(rootLayout);
+            primaryStage.setScene(rootScene);
+            primaryStage.setResizable(false);
 
-			addToTray();
+            addToTray();
 
-			primaryStage.getIcons().add(new Image(MainGui.class.getResource("/image/icon1.png").toString()));
-			primaryStage.show();
+            primaryStage.getIcons().add(new Image(MainGui.class.getResource("/image/icon1.png").toString()));
+            primaryStage.show();
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			// log.error("IOException occurred when load MainLayout.fxml..", e);
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * set the root layout dragable
-	 * 
-	 * @param n
-	 *            the instance of Node
-	 */
-	private void setDragable(Node n) {
-		n.setOnMousePressed(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				xOffset = primaryStage.getX() - event.getScreenX();
-				yOffset = primaryStage.getY() - event.getScreenY();
-			}
-		});
+    /**
+     * set the root layout dragable
+     * 
+     * @param n
+     *            the instance of Node
+     */
+    private void setDragable(Node n) {
+        n.setOnMousePressed((event) -> {
+            xOffset = primaryStage.getX() - event.getScreenX();
+            yOffset = primaryStage.getY() - event.getScreenY();
+        });
+        n.setOnMouseDragged((event) -> {
+            primaryStage.setX(event.getScreenX() + xOffset);
+            primaryStage.setY(event.getScreenY() + yOffset);
+        });
+    }
 
-		n.setOnMouseDragged(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				primaryStage.setX(event.getScreenX() + xOffset);
-				primaryStage.setY(event.getScreenY() + yOffset);
-			}
-		});
-	}
+    /**
+     * set the root layout dragable
+     * 
+     * @param the
+     *            instance of Pane
+     */
+    private void setDragable(Pane p) {
+        setDragable((Node) p);
+        for (Node n : p.getChildren()) {
+            setDragable(n);
+        }
+    }
 
-	/**
-	 * set the root layout dragable
-	 * 
-	 * @param the
-	 *            instance of Pane
-	 */
-	private void setDragable(Pane p) {
-		setDragable((Node) p);
-		for (Node n : p.getChildren()) {
-			setDragable(n);
-		}
-	}
+    private void addToTray() {
+        // ensure awt is initialized
+        java.awt.Toolkit.getDefaultToolkit();
 
-	private void addToTray() {
-		// ensure awt is initialized
-		java.awt.Toolkit.getDefaultToolkit();
+        // make sure system tray is supported
+        if (!java.awt.SystemTray.isSupported()) {
+            System.out.println("No system tray support!");
+            // log.warn("No system tray support!");
+        }
 
-		// make sure system tray is supported
-		if (!java.awt.SystemTray.isSupported()) {
-			System.out.println("No system tray support!");
-			// log.warn("No system tray support!");
-		}
+        final java.awt.SystemTray tray = java.awt.SystemTray.getSystemTray();
+        try {
 
-		final java.awt.SystemTray tray = java.awt.SystemTray.getSystemTray();
-		try {
+            java.awt.Image image = ImageIO.read(MainGui.class.getResource("/image/icon2.png"));
+            trayIcon = new TrayIcon(image);
+            trayIcon.addActionListener((e) -> Platform.runLater(() -> primaryStage.show()));
 
-			java.awt.Image image = ImageIO.read(MainGui.class.getResource("/image/icon2.png"));
-			trayIcon = new TrayIcon(image);
-			trayIcon.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					Platform.runLater(new Runnable() {
-						@Override
-						public void run() {
-							primaryStage.show();
-						}
-					});
-				}
-			});
+            java.awt.MenuItem openItem = new java.awt.MenuItem("Display");
+            openItem.addActionListener((e) -> Platform.runLater(() -> show()));
 
-			java.awt.MenuItem openItem = new java.awt.MenuItem("Display");
-			openItem.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					Platform.runLater(new Runnable() {
-						@Override
-						public void run() {
-							show();
-						}
-					});
-				}
-			});
+            java.awt.MenuItem exitItem = new java.awt.MenuItem("Exit");
+            exitItem.addActionListener((e) -> Manager.getInstance().terminate());
 
-			java.awt.MenuItem exitItem = new java.awt.MenuItem("Exit");
-			exitItem.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					Manager.getInstance().terminate();
-				}
-			});
+            PopupMenu popup = new PopupMenu();
+            popup.add(openItem);
+            popup.addSeparator();
+            popup.add(exitItem);
+            trayIcon.setPopupMenu(popup);
+            trayIcon.setToolTip("Not Connected");
+            tray.add(trayIcon);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
 
-			PopupMenu popup = new PopupMenu();
-			popup.add(openItem);
-			popup.addSeparator();
-			popup.add(exitItem);
-			trayIcon.setPopupMenu(popup);
-			trayIcon.setToolTip("Not Connected");
-			tray.add(trayIcon);
-		} catch (IOException e) {
-			e.printStackTrace();
-			// log.error("IOException occurred when addToTray..", e);
-		} catch (AWTException e) {
-			e.printStackTrace();
-			// log.error("AWTException occurred when addToTray..", e);
-		}
-	}
+    public void show() {
+        primaryStage.show();
+    }
 
-	public void show() {
-		primaryStage.show();
-	}
+    public void hide() {
+        primaryStage.hide();
+    }
 
-	public void hide() {
-		primaryStage.hide();
-	}
+    public void setTooltip(String message) {
+        if (trayIcon != null) {
+            trayIcon.setToolTip(message);
+        }
+    }
 
-	public void setTooltip(String message) {
-		if (trayIcon != null) {
-			trayIcon.setToolTip(message);
-		}
-	}
-
-	public void showNotification(String message) {
-		trayIcon.displayMessage("File Download Tools", message, java.awt.TrayIcon.MessageType.INFO);
-	}
+    public void showNotification(String message) {
+        trayIcon.displayMessage("File Download Tools", message, java.awt.TrayIcon.MessageType.INFO);
+    }
 }
